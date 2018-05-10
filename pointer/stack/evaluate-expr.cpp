d5dfa0b54a7d423ed11f90430220a06aca14ef40
Didik Kurniawan <didikunila@gmail.com>
2018-05-11 02:45:28 +0700

    expression to infix vector

--- pointer/stack/evaluate-expr.cpp
+++ pointer/stack/evaluate-expr.cpp
@@ -20,22 +20,106 @@ bool in_array(char mychar[], int n, char x)
 
 int main()
 {
-    int c;
+    string expr;
     char operand[] = "0123456789";
-    char operat[] = "+-/*%";
+    char operat[] = "+/*\%";
+    char minus = '-';
+    char kurung[] = "()";
+    string legal{""};
+    legal.append("0123456789");
+    legal.append("+/*\%");
+    legal.append("-()");
+    // puts(legal.c_str());
+    char *cstr = new char[legal.length() + 1];
+    strcpy(cstr, legal.c_str());
+    cout << "Masukan ekspresi aritmatika, lalu tekan ENTER \n";
+    getline(cin, expr);
 
-    puts("Masukan ekspresi aritmatika, lalu tekan ENTER ");
-    do
+    for (int i = 0; i < expr.length(); i++)
     {
-        c = getchar();
-        if (in_array(operand, strlen(operand), c))
-            cout << (char)c << " operand" << endl;
-        if (in_array(operat, strlen(operat), c))
-            cout << (char)c << " operator" << endl;
-        if (c == '(')
-            cout << (char)c << " left parenthesis" << endl;
-        if (c == ')')
-            cout << (char)c << " right parenthesis" << endl;
-    } while (c != '\n');
+        if (!in_array(cstr, strlen(cstr), expr.at(i)))
+        {
+            cerr << "Error expression in " << expr.at(i);
+            return -1;
+        }
+    }
+    if (in_array(operat, strlen(operat), expr.at(0)))
+    {
+        cerr << "Error: First character of expression is operator " << expr.at(0);
+        return -1;
+    }
+
+    // infix[0] = expr.at(0);
+    // for (int i = 1; i < expr.length(); i++)
+    // {
+    //     // cout << expr.at(i) << endl;
+    //     if (in_array(operand, strlen(operand), expr.at(i)))
+    //     {
+    //         cout << expr.at(i) << " operand" << endl;
+    //         infix += expr.at(i);
+    //     }
+    string buff{""};
+    vector<string> infix;
+
+    for (auto n : expr)
+    {
+        if (n == minus)
+            if (buff == "")
+            {
+                if (infix.back() == ")")
+                {
+                    string opr{""};
+                    opr += n;
+                    infix.push_back(opr);
+                }
+                else
+                    buff += n;
+            }
+            else
+            {
+                infix.push_back(buff);
+                buff = "";
+                string opr{""};
+                opr += n;
+                infix.push_back(opr);
+            }
+        if (in_array(kurung, strlen(kurung), n))
+            if (buff == "")
+            {
+                string opr{""};
+                opr += n;
+                infix.push_back(opr);
+            }
+            else
+            {
+                infix.push_back(buff);
+                buff = "";
+                string opr{""};
+                opr += n;
+                infix.push_back(opr);
+            }
+
+        if (in_array(operand, strlen(operand), n))
+            buff += n;
+        else if (in_array(operat, strlen(operat), n) && buff != "")
+        {
+            infix.push_back(buff);
+            buff = "";
+            string opr{""};
+            opr += n;
+            infix.push_back(opr);
+        }
+        else if (in_array(operat, strlen(operat), n) && buff == "" && infix.back() == ")")
+        {
+            string opr{""};
+            opr += n;
+            infix.push_back(opr);
+        }
+    }
+    if (buff != "")
+        infix.push_back(buff);
+
+    for (auto n : infix)
+        cout << n << " ";
     return 0;
 }
